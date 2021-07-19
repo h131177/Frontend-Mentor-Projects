4d5cf2ae330616338b7de898f67fbffc246d0403
h131177 <54102543+h131177@users.noreply.github.com>
2021-07-20 01:01:56 +0200

    play method and findWinner method

--- Rock-Paper-Scissors Game/gamecontroller.js
+++ Rock-Paper-Scissors Game/gamecontroller.js
@@ -8,6 +8,7 @@ class GameController {
         this.play = this.play.bind(this);
         this.playAgain = this.playAgain.bind(this);
         this.playComputer = this.playComputer.bind(this);
+        this.findWinner = this.findWinner.bind(this);
     }
 
     run() {
@@ -19,10 +20,32 @@ class GameController {
 
     play(e) {
         console.log("Playing");
+        //Get your choice
         console.log('You: ' + e.path[0].alt);
         const choice = e.path[0].alt;
+
+        //Get computer choice
         const computerChoice = this.playComputer();
         console.log('Computer: ' + computerChoice);
+
+        this.findWinner();
+
+        //toggle hidden class
+        const triangle = this.rootElement.querySelector(".playground .triangle");
+        triangle.classList.toggle("hidden");
+        const p1 = this.rootElement.querySelector(".playground p:nth-child(5)");
+        p1.classList.toggle("hidden");
+        const p2 = this.rootElement.querySelector(".playground p:nth-child(6)");
+        p2.classList.toggle("hidden");
+        const rock = this.rootElement.querySelector(".playground img:nth-child(4)");
+        rock.classList.toggle("hidden");
+        const resultDiv = this.rootElement.querySelector(".result");
+        resultDiv.classList.toggle("hidden");
+
+        //Switch some css styling
+        const playground = this.rootElement.querySelector(".playground");
+        playground.style.gridAutoRows = "minmax(auto, 190px)";
+        playground.style.marginBottom = "0px";
     }
 
     playAgain() {
@@ -41,6 +64,12 @@ class GameController {
         }
         console.log(number);
         return computer;
+    }
+
+    findWinner() {
+        let result = 'YOU WIN';
+        //add a switch statement here
+        this.rootElement.querySelector(".result [data-result]").textContent = result;
     }
 }
 
