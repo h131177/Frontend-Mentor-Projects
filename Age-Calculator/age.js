e045b5def67325228b8d6928acab62d984f375cc
h131177 <54102543+h131177@users.noreply.github.com>
2023-07-01 22:45:43 +0200

    checking if field is empty

--- Age-Calculator/age.js
+++ Age-Calculator/age.js
@@ -1,1 +1,32 @@
 "use strict"
+
+//inputs
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+//lables
+const dayLabel = document.querySelector(".day label");
+
+const button = document.querySelector("button");
+
+function calculate() {
+    let invalidInput = false;
+    if(day.value.trim() == "") {
+        day.classList.add('invalidInput');
+        dayLabel.classList.add('red');
+    } else {
+        day.classList.remove('invalidInput');
+        dayLabel.classList.remove('red');
+    }
+}
+
+button.addEventListener('click', calculate);
+
+/*
+Any field is empty when the form is submitted
+The day number is not between 1-31
+The month number is not between 1-12
+The year is in the future
+The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+*/
